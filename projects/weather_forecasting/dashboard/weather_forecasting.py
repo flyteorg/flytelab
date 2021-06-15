30c5b5cfa5434f8ddd85513c263f439cbe904e78
cosmicBboy <niels.bantilan@gmail.com>
2021-06-15 11:20:06 -0400

    update streamlit app
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- projects/weather_forecasting/dashboard/weather_forecasting.py
+++ projects/weather_forecasting/dashboard/weather_forecasting.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 import streamlit as st
 
@@ -22,7 +23,7 @@ st.set_page_config(
     page_title="flytelab - weather forecasts",
     page_icon=LOGO,
 )
-client = SynchronousFlyteClient("demo.nuclyde.io")
+client = SynchronousFlyteClient("sandbox.uniondemo.run")
 
 _, _, col, *_ = st.beta_columns(5)
 with col:
@@ -35,12 +36,28 @@ that was trained using [flyte](https://flyte.org/). For more information
 see the [flytelab weather forecasting project](https://github.com/flyteorg/flytelab/tree/main/projects/weather_forecasting).
 """
 
+launch_plan_map = {
+    "seattle_wa_usa": "seattle_weather_forecast",
+    "atlanta_ga_usa": "atlanta_weather_forecast",
+}
+
+city_label_map = {
+    "atlanta_ga_usa": "Atlanta, GA USA",
+    "seattle_wa_usa": "Seattle, WA USA",
+}
+
+selected_city = st.selectbox(
+    "Select a City",
+    options=["atlanta_ga_usa", "seattle_wa_usa"],
+    format_func=lambda x: city_label_map[x]
+)
+
 executions, _ = client.list_executions_paginated(
     "flytelab",
     "development",
     limit=1,
     filters=[
-        filters.Equal("workflow.name", "app.workflow.run_pipeline"),
+        filters.Equal("launch_plan.name", launch_plan_map[selected_city]),
         filters.Equal("phase", "SUCCEEDED"),
     ],
     sort_by=Sort.from_python_std("desc(execution_created_at)"),
@@ -57,11 +74,14 @@ with st.beta_expander("Model Metadata"):
     ```
     """)
 
-st.markdown("""
-## Atlanta, GA USA
+st.markdown(f"""
+## {city_label_map[selected_city]}
 ---
 """)
+
 for prediction in forecast.predictions[::-1]:
+    if prediction.date.date() < datetime.datetime.now().date():
+        continue
     st.markdown(f"### {prediction.date.date().strftime('%m/%d/%Y')}")
     st.markdown(f"🌡 **Mean Temperature**: {prediction.value:0.02f} °C")
     st.markdown("---")
