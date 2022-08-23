faaf72a6b4ed6efded1676aa311af4e64f8dff17
amaleelhamri <48512530+amaleelhamri@users.noreply.github.com>
2022-08-23 22:37:45 +0200

    [adorable-unicorns23] submission: What's cooking good looking (#18)
    * initial setup
    
    * github workflow for deployement on push
    
    * feat: init tasks and workflow
    
    * remove: unused pandas in requirements
    
    * created tasks for model training
    
    * removed tuples from jsonl training data example(
    
    * training data entities are stored as list of lists
    
    * fixed errors in workflow
    
    * isorted / flake8
    
    * fixed s3fs in requirements for ci
    
    * fixed spacy training
    
    * changed spacy model version in requirements
    
    * added flake8 file
    
    * put back 1000 as max_results value
    
    * minor print added
    
    * fix: add click
    
    * fix: dl files only
    
    * added model training iterations in config.json
    
    * feat: convert to label studio format
    
    * minor kwargs fix and fixed click version to 7.1.2  as required by spacy 3.2.3
    
    * made config file path relative
    
    * moved config.json and fixed broken gcs download
    
    * changed if list into is len(list)>0
    
    * moved retrieve_train_data_path and renamed it
    
    * moved retrieve_train_data_path and renamed it
    
    * add runner key to docker arg
    
    * lowercase
    
    * update dockerfile for credentials
    
    * fix typo
    
    * Update Dockerfile
    
    * dockerfile change
    
    * test
    
    * test without build args
    
    * base64 creds
    
    * removed gcs download, and load data using pandas and gcsfs directly
    
    * Updated gcsfs version to avoid conflicts with s3fs
    
    * add: initial readme
    
    * split: 2 pipelines (train and apply
    
    * fix: code format
    
    * add: docstring
    
    * add: upload result to gcs
    
    * add: pipelines explanation
    
    * typo
    
    * add: dynamic in train_ner_workflow
    
    * debug: train ner workflow
    
    * fix: code format
    
    * rename: tasks > labelstudio_tasks
    
    * fix: main conflicts
    
    * fix: image in readme
    
    * add: images in readme
    
    * add: info in print
    
    * updated cd readme section
    
    * add: main docstring in training pipeline
    
    * add: pipeline screenshots
    
    * feat: add label studio screen
    
    * fix: readme typos
    
    * fix/ Fixed comments made on Flyte MR
    
    * fix/ Isorted
    
    * fix/ Fixed return in train_if_necessary dynamic
    
    Co-authored-by: Karim SI LARBI <karim.si-larbi@artefact.com>
    Co-authored-by: lrdsc <louis.rousselot-de-saint-ceran@artefact.com>
    Co-authored-by: Louis Rousselot de Saint Ceran <78730750+louisRDSC@users.noreply.github.com>
    Co-authored-by: sacha.lasry <sacha.lasry@artefact.com>
    Co-authored-by: Hugo VASSELIN <hugo.vasselin@artefact.com>
    Co-authored-by: sachalasry-artefact <66633561+sachalasry-artefact@users.noreply.github.com>
    Co-authored-by: sacha.lasry <sacha.lasry.ext@orange.com>

---
+++ projects/whats_cooking_good_looking/README.md
@@ -0,0 +1,91 @@
+# Whats_cooking_good_looking
+
+## Problem statement
+
+The world of beauty is in constant evolution. New molecules, new brands, new discovered benefits. Innovating is time consuming, especially when you are a large corporation. So a possible strategy is to find "indie brands" that innovate, and that the public likes, and buy them out.
+Social networks are a perfect place to talk about beauty, and therefore a great place to discover such brands.
+The problem then lies in, how to detect the new brands and trendy products or benefits, to help the merger division finding the right buy?
+
+## Target Solution implementation
+
+<p align="center">
+    <img src="docs/target_pipeline.png" />
+</p>
+
+In order to extract brands in a tweet, we implemented a NER (named entity recognition) pipeline that:
+1. Retrieves tweets related to beauty based on keywords
+2. Applies a pretrained NER model to those posts
+3. Sends model results to a labelling interface and waits for a manual annotation to check given results
+4. Computes evaluation metrics (so far only accuracy but it would be interesting to compute precision and recall as well)
+5. 1. If metrics are good enough (defined by a business standard), the pipeline end
+5. 2. If metrics are not good enough, it sends those labeled posts into a training task and goes back into the same piece of pipeline composed of steps 2 - 3 - 4
+
+
+## Actual Solution implementation
+
+<p align="center">
+    <img src="docs/actual_pipeline.png" />
+</p>
+
+The project is cut into 3 steps:
+
+### 1. NER application pipeline
+1. Retrieves tweets related to beauty based on keywords
+2. Applies a NER model to those posts
+3. Sends model results in a format that could load into Label Studio in a GCS bucket
+
+To run this pipeline locally please run
+```python whats_cooking_good_looking/apply_ner_workflow.py```
+
+<p align="center">
+    <img src="docs/apply_pipeline.png" />
+</p>
+
+### 2. Manual labelling part in Label Studio
+
+<p align="center">
+    <img src="docs/label_studio.png" />
+</p>
+
+
+### 3. NER training pipeline
+1. Retrieves labelled tasks (Label Studio output)
+2. Computes model accuracy based on those labelled observations
+3. 1. If metrics are good enough, pipeline stops
+3. 2. If metrics are not good enough, labelled tasks are used as input to train a new NER model
+
+The goal was to create a feedback loop where it was possible to iterate by training NER models based on new manual annotations. We chose to cut into 2 pipelines to get rid of the network constraints that we would have to handle and that were not evaluated in the scope of this hackaton.
+
+To run this pipeline locally please run
+```python whats_cooking_good_looking/train_ner_workflow.py```
+
+<p align="center">
+    <img src="docs/train_pipeline.png" />
+</p>
+## Pipeline deployment
+
+The Goal of this pipeline is deploy workflows automatically to the playground on pushed commits
+
+### Steps and flow
+the steps are :
+1. Build the dockerfile of our project
+2. Push the build docker image to the container registery of our GCP project
+3. serialize our workflows using the pyflyte cli
+4. register the packages to our playgroud project
+
+Step 3 and 4 are using external docker based github actions that we made ourselves : [serialize](https://github.com/louisRDSC/FlyteSerializeAction), [register](https://github.com/louisRDSC/flyteRegisterAction)
+
+for all branches except main, the workflows are registered in the development environment. For main the workflows are registered in the staging environment. Future iterations could register them in the production evironment when a tag is created.
+
+In develpment each version of a workflow is named like followed : \<branch name\>-\<datetime\>.
+This allows ourself to easily recognize our work while working simultaneously on different branches.
+
+In staging each version of a workflow is named like followed : \<datetime\>
+
+### How to make it work
+
+4 secrets are required for the pipeline to work :
+- ClIENT_ID : the ID to authenticate with the playground
+- ClIENT_SECRET : The secret to authenticate with the playgroud
+- RUNNER_KEY : The json key encoded in BASE64 of a GCP service account with read and write rights on the gcp bucket where the data il pulled and pushed from.
+- SERVICE_ACCOUNT_KEY : The json key of a service account with write rights on the container registery where we are pushing our images
---
+++ projects/whats_cooking_good_looking/whats_cooking_good_looking/apply_ner_workflow.py
@@ -0,0 +1,184 @@
+import json
+from pathlib import Path
+from typing import List
+
+import spacy
+from flytekit import Resources, task, workflow
+from snscrape.modules.twitter import TwitterSearchScraper
+
+from whats_cooking_good_looking.utils import (doc_to_spans, download_from_gcs,
+                                              load_config, upload_to_gcs)
+
+SPACY_MODEL = {"en": "en_core_web_sm"}
+
+CACHE_VERSION = "2.2"
+request_resources = Resources(cpu="1", mem="500Mi", storage="500Mi")
+limit_resources = Resources(cpu="2", mem="1000Mi", storage="1000Mi")
+
+
+@task
+def get_tweets_list(
+    keyword_list: List[str], lang: str = "en", max_results: int = 1000
+) -> str:
+    """Collects `max_results` tweets mentioning any of the words in `keywords_list` written in language `lang`.
+
+    Args:
+        keyword_list (List[str]): List of keywords that tweets must mention at least one of.
+        lang (str, optional): Language in which tweets must be written(iso-code). Defaults to "en".
+        max_results (int, optional): Number of maximum tweets to retrieve. Defaults to 1000.
+
+    Returns:
+        str: json dumped results with following shape
+            [
+                {
+                    "date": "2022-03-25 16:23:01+00:00,
+                    "tweet_id": "XXXXXXX",
+                    "text": "some tweet",
+                    "username": "some user"
+                },
+            ]
+    """
+    keywords_query = " OR ".join(keyword_list)
+    query = f"({keywords_query}) lang:{lang}"
+    tweets_list = []
+    for tweet_idx, tweet_post in enumerate(TwitterSearchScraper(query).get_items()):
+        if tweet_idx == max_results:
+            break
+        tweets_list.append(
+            {
+                "date": str(tweet_post.date),
+                "tweet_id": str(tweet_post.id),
+                "text": str(tweet_post.content),
+                "username": str(tweet_post.username),
+            }
+        )
+    return json.dumps(tweets_list)
+
+
+@task
+def load_model(
+    lang: str,
+    from_gcs: bool,
+    gcs_bucket: str,
+    gcs_source_blob_name: str,
+) -> spacy.Language:
+    """Loads spacy model either from gcs if specified or given the source language.
+
+    Args:
+        lang (str): Language in which tweets must be written(iso-code).
+        from_gcs (bool): True if needs to download custom spacy model from gcs.
+        gcs_bucket (str): bucket name where to retrieve spacy model if from_gcs.
+        gcs_source_blob_name (str): blob name where to retrieve spacy model if from_gcs.
+
+    Returns:
+        Language: spacy model.
+    """
+    if from_gcs:
+        Path("tmp").mkdir(parents=True, exist_ok=True)
+        output_filename = download_from_gcs(
+            gcs_bucket, gcs_source_blob_name, "tmp", explicit_filepath=True
+        )[0]
+        nlp = spacy.load(output_filename)
+    else:
+        model_name = SPACY_MODEL[lang]
+        nlp = spacy.load(model_name)
+    return nlp
+
+
+@task
+def apply_model(
+    nlp: bytes, tweets_list: str, bucket_name: str, source_blob_name: str
+) -> str:
+    """Applies spacy model to each tweet to extract entities from and convert them into
+    Label studio task format.
+
+    Args:
+        nlp (Language): Spacy model to use for inference.
+        tweets_list (str): json dumped list of tweets.
+        bucket_name (str): Name of the GCS bucket to upload to.
+        source_blob_name (str): File name of GCS uploaded file.
+
+    Returns:
+        str: json dumped results with following shape
+            [
+                {
+                    "date": "2022-03-25 16:23:01+00:00,
+                    "tweet_id": "XXXXXXX",
+                    "text": "some tweet",
+                    "username": "some user"
+                    "entities": [
+                        {
+                        "label": "some label",
+                        "start_char": "index beginning char entity",
+                        "end_char": "index end char entity"
+                        },
+                    ]
+
+                }
+            ]
+    """
+    entities = set()
+    labelstudio_tasks = []
+    model_name = SPACY_MODEL["en"]
+    for tweet in json.loads(tweets_list):
+        predictions = []
+        text = tweet["text"]
+        doc = nlp(text)
+        spans, ents = doc_to_spans(doc)
+        entities |= ents
+        predictions.append({"model_version": model_name, "result": spans})
+        labelstudio_tasks.append({"data": {"text": text}, "predictions": predictions})
+    with open("tasks.json", mode="w") as f:
+        json.dump(labelstudio_tasks, f, indent=2)
+    json_labelstudio_tasks = json.dumps(labelstudio_tasks)
+    upload_to_gcs(
+        bucket_name, source_blob_name, json_labelstudio_tasks, content_type=None
+    )
+    return json_labelstudio_tasks
+
+
+@workflow
+def main() -> str:
+    """Main workflow searching for entities in beauty related tweets.
+
+    Returns:
+        str: json dumped results with following shape
+            [
+                {
+                    "date": "2022-03-25 16:23:01+00:00,
+                    "tweet_id": "XXXXXXX",
+                    "text": "some tweet",
+                    "username": "some user"
+                    "entities": [
+                        {
+                        "label": "some label",
+                        "start_char": "index beginning char entity",
+                        "end_char": "index end char entity"
+                        },
+                    ]
+
+                }
+            ]
+    """
+    config = load_config("apply")
+    tweets_list = get_tweets_list(
+        keyword_list=config["keyword_list"],
+        lang=config["lang"],
+        max_results=config["max_results"],
+    )
+    nlp = load_model(
+        lang=config["lang"],
+        from_gcs=config["from_gcs"],
+        gcs_bucket=config["bucket_name"],
+        gcs_source_blob_name=config["gcs_spacy_model_blob_name"],
+    )
+    return apply_model(
+        nlp=nlp,
+        tweets_list=tweets_list,
+        bucket_name=config["bucket_name"],
+        source_blob_name=config["applied_model_output_blob_name"],
+    )
+
+
+if __name__ == "__main__":
+    print(f"Applied model: {main()}")
